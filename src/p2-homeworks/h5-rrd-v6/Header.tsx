228397b7725d7ab8086dce8b4d10b63561b6bda4
Aleksandr_UB <bogdanov777000@mail.ru>
2022-06-25 11:24:33 +0300

    5- TZ ready

--- src/p2-homeworks/h5-rrd-v6/Header.tsx
+++ src/p2-homeworks/h5-rrd-v6/Header.tsx
@@ -1,34 +1,24 @@
 import React from 'react'
 import {NavLink} from 'react-router-dom';
 import {PATH} from './Pages';
+import  './Header.css';
 
 function Header() {
     return (
-        <div className="header">
-            <NavLink
-                className={({isActive}) => {
-                    return isActive ? 'act' : 'def'
-                }}
-                style={(params) => {
-                return {color: params.isActive ? 'lime' : 'white'}
-            }}
-                to={PATH.PRE_JUNIOR}> pre-junior </NavLink>
-            <NavLink
-                className={({isActive}) => {
-                    return isActive ? 'act' : 'def'
-                }}
-                style={(params) => {
-                return {color: params.isActive ? 'lime' : 'white'}
-            }}
-                to={PATH.JUNIOR}> junior </NavLink>
-            <NavLink
-                className={({isActive}) => {
-                    return isActive ? 'act' : 'def'
-                }}
-                style={(params) => {
-                return {color: params.isActive ? 'lime' : 'white'}
-            }}
-                to={PATH.JUNIOR_PLUS}> junior-plus+ </NavLink>
+        <div className='header'>
+            <div className='toggle'>x</div>
+            <div className='box'>
+                <NavLink
+                    className={({isActive}) => isActive ? 'act' : 'noEct'}
+                    to={PATH.PRE_JUNIOR}> pre-junior </NavLink>
+                <NavLink
+                    className={({isActive}) => isActive ? 'act' : 'noEct'}
+                    to={PATH.JUNIOR}> junior </NavLink>
+                <NavLink
+                    className={({isActive}) => isActive ? 'act' : 'noEct'}
+                    to={PATH.JUNIOR_PLUS}> junior-plus+ </NavLink>
+            </div>
+
         </div>
     )
 }
