3713252680db8bed20d713eabf3e57bb737eded6
Aleksandr_UB <bogdanov777000@mail.ru>
2022-07-31 12:44:32 +0300

    10 - Preloader

--- src/p2-homeworks/h10/bll/loadingReducer.ts
+++ src/p2-homeworks/h10/bll/loadingReducer.ts
@@ -1,14 +1,19 @@
 const initState = {
-
+    isLoading: false
+}
+export type LoadingActionType = {
+    type: 'TOGGLE_IS_LOADING'
+    isLoading: boolean
 }
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
+export const loadingReducer = (state = initState, action: LoadingActionType): typeof initState => {
     switch (action.type) {
-        case '': {
-            return state
-        }
+        case 'TOGGLE_IS_LOADING':
+            return {
+                ...state, isLoading: action.isLoading
+            }
         default: return state
     }
 }
 
-export const loadingAC = (): any => {} // fix any
+export const loadingAC = (isLoading: boolean): LoadingActionType => ({type: 'TOGGLE_IS_LOADING', isLoading} as const)
